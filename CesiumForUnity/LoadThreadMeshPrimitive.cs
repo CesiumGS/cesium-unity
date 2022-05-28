8b77c018825e7be3274893a26acb5e3f89e0a19d
Kevin Ring <kevin@kotachrome.com>
2022-05-28 10:31:32 +1000

    Slowly moving toward building meshes.

---
+++ CesiumForUnity/LoadThreadMeshPrimitive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CesiumForUnity
+{
+    /// <summary>
+    /// Holds details of a glTF mesh primitive populated on the load thread
+    /// and ready for use on the main thread
+    /// </summary>
+    internal class LoadThreadMeshPrimitive
+    {
+        NativeArray<Vector3>? positions;
+        NativeArray<Vector3>? normals;
+        NativeArray<Vector4>? tangents;
+        NativeArray<Vector2>[]? uvs;
+        NativeArray<Color>? colors;
+        NativeArray<Color32>? colors32;
+
+        // TODO: with something like the below, we can probably use the glTF buffers directly and avoid a copy.
+        //VertexAttributeDescriptor[]? attributeDescriptors;
+        //NativeArray<byte>[]? buffers;
+        //NativeArray<int>? triangles;
+    }
+}
--- CesiumForUnity/UnityPrepareRendererResources.cs
+++ CesiumForUnity/UnityPrepareRendererResources.cs
@@ -1,16 +1,29 @@
 ﻿
 using Cesium3DTilesSelection;
 using CesiumGltf;
-using glm;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 namespace CesiumForUnity
 {
     internal class UnityPrepareRendererResources : PrepareRendererResources
     {
-        public override object? prepareInLoadThread(Model model, dmat4 transform)
+        public override object? prepareInLoadThread(Model model, CesiumUtility.Matrix4 transform)
         {
             Debug.Log("prepareInLoadThread");
+            ScenePrimitive[] primitives = model.GetPrimitivesInScene(-1);
+
+            for (int i = 0; i < primitives.Length; ++i)
+            {
+                Debug.Log("Primitive! " + primitives[i].transform);
+            }
+            //var a = new NativeArray<float>(5, Allocator.Persistent);
+            //unsafe
+            //{
+            //    void* p = NativeArrayUnsafeUtility.GetUnsafePtr(a);
+            //}
+            //model.GetAllPrimitives()
             return null;
         }
 
