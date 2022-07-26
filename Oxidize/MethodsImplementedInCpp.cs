f005d4c1873cd9c1cb28e4e25533b6bb8e5d750a
Kevin Ring <kevin@kotachrome.com>
2022-07-26 22:51:34 +1000

    Clean up implemented-in-C++ code generation.

--- Oxidize/MethodsImplementedInCpp.cs
+++ Oxidize/MethodsImplementedInCpp.cs
@@ -8,7 +8,46 @@ namespace Oxidize
         public static void Generate(CppGenerationContext context, GenerationItem item, GeneratedResult result)
         {
             Debug.Assert(result.CppImplementationInvoker != null);
+            if (result.CppImplementationInvoker == null)
+                return;
 
+            // Add functions to create and destroy the implementation class instance.
+            CppType wrapperType = result.CppDefinition.Type;
+            CppType implType = result.CppImplementationInvoker.ImplementationType;
+            CppType objectHandleType = CppObjectHandle.GetCppType(context);
+
+            result.CppImplementationInvoker.Functions.Add(new(
+                content:
+                    $$"""
+                    void* {{result.CppDefinition.Type.GetFullyQualifiedName(false).Replace("::", "_")}}_Create(void* handle) {
+                      const {{wrapperType.GetFullyQualifiedName()}} wrapper({{objectHandleType.GetFullyQualifiedName()}}(handle));
+                      return new {{implType.GetFullyQualifiedName()}}(wrapper);
+                    }
+                    """,
+                typesReferenced: new[]
+                {
+                    new CppTypeReference(wrapperType, true),
+                    new CppTypeReference(implType, true),
+                    new CppTypeReference(objectHandleType, true)
+                }));
+
+            result.CppImplementationInvoker.Functions.Add(new(
+                content:
+                    $$"""
+                    void* {{result.CppDefinition.Type.GetFullyQualifiedName(false).Replace("::", "_")}}_Destroy(void* handle, void* pImpl) {
+                      const {{wrapperType.GetFullyQualifiedName()}} wrapper({{objectHandleType.GetFullyQualifiedName()}}(handle));
+                      auto pImplTyped = reinterpret_cast<{{implType.GetFullyQualifiedName()}}*>(pImpl);
+                      pImplTyped->Destroy(wrapper);
+                    }
+                    """,
+                typesReferenced: new[]
+                {
+                    new CppTypeReference(wrapperType, true),
+                    new CppTypeReference(implType, true),
+                    new CppTypeReference(objectHandleType, true)
+                }));
+
+            // Add functions for other methods.
             foreach (IMethodSymbol method in item.methodsImplementedInCpp)
             {
                 GenerateMethod(context, item, result, method);
@@ -42,19 +81,22 @@ namespace Oxidize
             var callParameterList = new[] { "wrapper" }.Concat(parameters.Select(parameter => parameter.Type.GetConversionFromInteropType(context, parameter.Name)));
             string callParameterListString = string.Join(", ", callParameterList);
 
+            CppType objectHandleType = CppObjectHandle.GetCppType(context);
+
             result.CppImplementationInvoker.Functions.Add(new(
                 content: $$"""
                     {{interopReturnType.GetFullyQualifiedName()}} {{name}}({{parameterListString}}) {
-                        const {{wrapperType.GetFullyQualifiedName()}} wrapper(ObjectHandle(handle));
-                        auto pImplTyped = reinterpret_cast<{{implType.GetFullyQualifiedName()}}*>(pImpl);
-                        pImplTyped->{{method.Name}}({{callParameterListString}});
+                      const {{wrapperType.GetFullyQualifiedName()}} wrapper({{objectHandleType.GetFullyQualifiedName()}}(handle));
+                      auto pImplTyped = reinterpret_cast<{{implType.GetFullyQualifiedName()}}*>(pImpl);
+                      pImplTyped->{{method.Name}}({{callParameterListString}});
                     }
                     """,
                 typesReferenced: new[]
                 {
                     new CppTypeReference(wrapperType, true),
                     new CppTypeReference(implType, true),
-                    new CppTypeReference(returnType, true)
+                    new CppTypeReference(returnType, true),
+                    new CppTypeReference(objectHandleType, true)
                 }.Concat(parameters.Select(parameter => new CppTypeReference(parameter.Type)))
                 .Concat(parameters.Select(parameter => new CppTypeReference(parameter.InteropType)))
             ));
