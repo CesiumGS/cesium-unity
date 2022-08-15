b914a0ff2f29fd6a799f6e1601b8f17fc94ac2a4
Kevin Ring <kevin@kotachrome.com>
2022-08-16 08:44:02 +1000

    Remove unused code.

--- CesiumForUnityNative/src/UnityTaskProcessor.cpp
+++ CesiumForUnityNative/src/UnityTaskProcessor.cpp
@@ -7,28 +7,6 @@ using namespace DotNet;
 
 namespace CesiumForUnity {
 
-namespace {
-
-class TaskRunner /*: public System::Action<>*/ {
-public:
-  TaskRunner(std::function<void()>&& f) : _f(std::move(f)) {}
-
-  virtual void operator()() /*override*/ {
-    try {
-      this->_f();
-      delete this;
-    } catch (...) {
-      delete this;
-      throw;
-    }
-  }
-
-private:
-  std::function<void()> _f;
-};
-
-} // namespace
-
 void UnityTaskProcessor::startTask(std::function<void()> f) {
   System::Threading::Tasks::Task::Run(f);
 }
