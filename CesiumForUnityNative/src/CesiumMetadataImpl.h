1f697fa1948bc9e6286af59bcfd1f70505fb769e
Joseph Kaile <joseph.kaile@simblocks.io>
2022-10-03 14:07:41 -0400

    convert numeric/boolean/string-view to string

--- CesiumForUnityNative/src/CesiumMetadataImpl.h
+++ CesiumForUnityNative/src/CesiumMetadataImpl.h
@@ -13,6 +13,49 @@ class CesiumMetadata;
 
 namespace CesiumForUnityNative {
 
+      using ValueType = std::variant<
+      std::monostate,
+      int8_t,
+      uint8_t,
+      int16_t,
+      uint16_t,
+      int32_t,
+      uint32_t,
+      int64_t,
+      uint64_t,
+      float,
+      double,
+      bool,
+      std::string_view,
+      CesiumGltf::MetadataArrayView<int8_t>,
+      CesiumGltf::MetadataArrayView<uint8_t>,
+      CesiumGltf::MetadataArrayView<int16_t>,
+      CesiumGltf::MetadataArrayView<uint16_t>,
+      CesiumGltf::MetadataArrayView<int32_t>,
+      CesiumGltf::MetadataArrayView<uint32_t>,
+      CesiumGltf::MetadataArrayView<int64_t>,
+      CesiumGltf::MetadataArrayView<uint64_t>,
+      CesiumGltf::MetadataArrayView<float>,
+      CesiumGltf::MetadataArrayView<double>,
+      CesiumGltf::MetadataArrayView<bool>,
+      CesiumGltf::MetadataArrayView<std::string_view>>;
+
+      static std::string getString(const ValueType& value, const std::string& defaultValue){
+        return std::visit([defaultValue](auto&& arg){
+            using T = decltype(arg);
+            if constexpr(CesiumGltf::IsMetadataNumeric<T>::value){
+                return std::to_string(arg);
+            }
+            else if constexpr(CesiumGltf::IsMetadataBoolean<T>::value){
+                return arg ? "true" : "false";
+            }
+            else if constexpr(CesiumGltf::IsMetadataString<T>::value){
+                    return std::string(arg);
+            }
+           return defaultValue;
+        }, value);
+      }
+
    using PropertyType = std::variant<
       CesiumGltf::MetadataPropertyView<int8_t>,
       CesiumGltf::MetadataPropertyView<uint8_t>,
