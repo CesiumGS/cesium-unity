d7e56010e4d9e6f5043f031f7293ca3c7149d64d
Joseph Kaile <joseph.kaile@simblocks.io>
2022-10-04 15:37:42 -0400

    remove test property

--- CesiumForUnity/CesiumMetadata.cs
+++ CesiumForUnity/CesiumMetadata.cs
@@ -8,21 +8,7 @@ namespace CesiumForUnity
     [ReinteropNativeImplementation("CesiumForUnityNative::CesiumMetadataImpl", "CesiumMetadataImpl.h")]
     public partial class CesiumMetadata : MonoBehaviour
     {
-        [SerializeField]
-        [Header("test")]
-        [Tooltip("test 2")]
-        private double _test = 234;
-
-        public double test 
-        {
-            get => this._test;
-            set
-            {
-                this._test = value;
-            }
-        }
-
-        public partial void loadMetadata(Transform transform, int triangleIndex);
+       public partial void loadMetadata(Transform transform, int triangleIndex);
 
         private partial int getNumberOfProperties();
 
