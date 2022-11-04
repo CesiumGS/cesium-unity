db6c20dee143642b68e83d577e79bef503f1cb48
Kevin Ring <kevin@kotachrome.com>
2022-11-04 16:34:46 +1100

    Fix up some doc comments.

--- Runtime/CesiumSubLevel.cs
+++ Runtime/CesiumSubLevel.cs
@@ -8,7 +8,7 @@ namespace CesiumForUnity
     public class CesiumSubLevel : MonoBehaviour, INotifyOfChanges
     {
         [SerializeField]
-        [Tooltip("The set of coordinates that authoritatively define the origin of this georeference.")]
+        [Tooltip("The set of coordinates that authoritatively define the origin of this sub-level.")]
         [NotifyOfChanges]
         private CesiumGeoreferenceOriginAuthority _originAuthority = CesiumGeoreferenceOriginAuthority.LongitudeLatitudeHeight;
 
@@ -24,7 +24,7 @@ namespace CesiumForUnity
 
         [SerializeField]
         [Header("Origin")]
-        [Tooltip("The latitude of the origin in degrees, in the range [-90, 90].")]
+        [Tooltip("The latitude of the origin of this sub-level in degrees, in the range [-90, 90].")]
         [NotifyOfChanges]
         private double _latitude = 39.736401;
 
@@ -39,7 +39,7 @@ namespace CesiumForUnity
         }
 
         [SerializeField]
-        [Tooltip("The longitude of the origin in degrees, in the range [-180, 180].")]
+        [Tooltip("The longitude of the origin of this sub-level in degrees, in the range [-180, 180].")]
         [NotifyOfChanges]
         private double _longitude = -105.25737;
 
@@ -54,7 +54,7 @@ namespace CesiumForUnity
         }
 
         [SerializeField]
-        [Tooltip("The height of the origin in meters above the ellipsoid (usually WGS84). " +
+        [Tooltip("The height of the origin of this sub-level in meters above the ellipsoid (usually WGS84). " +
                  "Do not confuse this with a geoid height or height above mean sea level, which " +
                  "can be tens of meters higher or lower depending on where in the world the " +
                  "origin is located.")]
@@ -73,7 +73,7 @@ namespace CesiumForUnity
 
         [SerializeField]
         [Header("Position (Earth Centered, Earth Fixed)")]
-        [Tooltip("The Earth-Centered, Earth-Fixed X-coordinate of the origin of this georeference in meters.\n" +
+        [Tooltip("The Earth-Centered, Earth-Fixed X-coordinate of the origin of this sub-level in meters.\n" +
                  "In the ECEF coordinate system, the origin is at the center of the Earth \n" +
                  "and the positive X axis points toward where the Prime Meridian crosses the Equator.")]
         [NotifyOfChanges]
@@ -90,7 +90,7 @@ namespace CesiumForUnity
         }
 
         [SerializeField]
-        [Tooltip("The Earth-Centered, Earth-Fixed Y-coordinate of the origin of this georeference in meters.\n" +
+        [Tooltip("The Earth-Centered, Earth-Fixed Y-coordinate of the origin of this sub-level in meters.\n" +
                  "In the ECEF coordinate system, the origin is at the center of the Earth \n" +
                  "and the positive Y axis points toward the Equator at 90 degrees longitude.")]
         [NotifyOfChanges]
@@ -107,7 +107,7 @@ namespace CesiumForUnity
         }
 
         [SerializeField]
-        [Tooltip("The Earth-Centered, Earth-Fixed Z-coordinate of the origin of this georeference in meters.\n" +
+        [Tooltip("The Earth-Centered, Earth-Fixed Z-coordinate of the origin of this sub-level in meters.\n" +
          "In the ECEF coordinate system, the origin is at the center of the Earth \n" +
          "and the positive Z axis points toward the North pole.")]
         [NotifyOfChanges]
