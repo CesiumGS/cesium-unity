6b7bfb92b955bdcd47156ac273e6144fc0d69dea
Kevin Ring <kevin@kotachrome.com>
2023-01-17 21:44:38 +1100

    Add (failing) test.

--- Tests/TestCesiumGlobeAnchor.cs
+++ Tests/TestCesiumGlobeAnchor.cs
@@ -100,4 +100,46 @@ public class TestCesiumGlobeAnchor
         Assert.AreEqual(-45.0, anchor.latitude);
         Assert.AreEqual(101.0, anchor.height);
     }
+
+    [UnityTest]
+    public IEnumerator SettingPositionImmediatelyAfterAddingAnchorDoesNotAffectOrientation()
+    {
+        GameObject goGeoreference = new GameObject("Georeference");
+        CesiumGeoreference georeference = goGeoreference.AddComponent<CesiumGeoreference>();
+        georeference.longitude = -55.0;
+        georeference.latitude = 55.0;
+        georeference.height = 1000.0;
+
+        GameObject goAnchored = new GameObject("Anchored");
+        goAnchored.transform.parent = goGeoreference.transform;
+        goAnchored.transform.SetPositionAndRotation(new Vector3(100.0f, 200.0f, 300.0f), Quaternion.Euler(10.0f, 20.0f, 30.0f));
+
+        Assert.That(goAnchored.transform.rotation.eulerAngles.x, Is.EqualTo(10.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.y, Is.EqualTo(20.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.z, Is.EqualTo(30.0f).Using(FloatEqualityComparer.Instance));
+
+        CesiumGlobeAnchor anchor = goAnchored.AddComponent<CesiumGlobeAnchor>();
+
+        // Move to the opposite side of the globe in terms of longitude.
+        anchor.SetPositionLongitudeLatitudeHeight(125.0, 55.0, 1000.0);
+
+        // The orientation should be unaffected because the globe anchor hasn't been Sync'd yet.
+        Assert.That(goAnchored.transform.rotation.eulerAngles.x, Is.EqualTo(10.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.y, Is.EqualTo(20.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.z, Is.EqualTo(30.0f).Using(FloatEqualityComparer.Instance));
+
+        //// Wait for the start of a new frame, which will cause Start to be invoked.
+        yield return null;
+
+        // The orientation should still be unaffected
+        Assert.That(goAnchored.transform.rotation.eulerAngles.x, Is.EqualTo(10.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.y, Is.EqualTo(20.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.z, Is.EqualTo(30.0f).Using(FloatEqualityComparer.Instance));
+
+        // But now if we move it, the orientation will change, too.
+        anchor.SetPositionLongitudeLatitudeHeight(105.0, 55.0, 1000.0);
+        Assert.That(goAnchored.transform.rotation.eulerAngles.x, Is.Not.EqualTo(10.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.y, Is.Not.EqualTo(20.0f).Using(FloatEqualityComparer.Instance));
+        Assert.That(goAnchored.transform.rotation.eulerAngles.z, Is.Not.EqualTo(30.0f).Using(FloatEqualityComparer.Instance));
+    }
 }
