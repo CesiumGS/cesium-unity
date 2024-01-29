cf9b5232700933d0a37ac6640af619a80147a518
Ashley Rogers <ashley.rogers@cesium.com>
2024-01-29 15:00:33 -0500

    Format code

--- native~/Runtime/src/CesiumGlobeFlightPathImpl.h
+++ native~/Runtime/src/CesiumGlobeFlightPathImpl.h
@@ -19,7 +19,8 @@ namespace CesiumForUnityNative {
 
 class CesiumGlobeFlightPathImpl {
 public:
-  CesiumGlobeFlightPathImpl(const DotNet::CesiumForUnity::CesiumGlobeFlightPath& path);
+  CesiumGlobeFlightPathImpl(
+      const DotNet::CesiumForUnity::CesiumGlobeFlightPath& path);
   ~CesiumGlobeFlightPathImpl();
 
   bool CreateFromEarthCenteredEarthFixedCoordinates(
@@ -37,7 +38,8 @@ public:
       double percentage,
       double additionalHeight) const;
 
-  double GetLength(const DotNet::CesiumForUnity::CesiumGlobeFlightPath& path) const;
+  double
+  GetLength(const DotNet::CesiumForUnity::CesiumGlobeFlightPath& path) const;
 
 private:
   std::unique_ptr<CesiumGeospatial::GlobeFlightPath> _flightPath;
