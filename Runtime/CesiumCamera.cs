08bae4fcbeb9ce2c9c047c57925b8be42a31a851
nicolas.raggueneau <nicolas.raggueneau@airbus.com>
2024-02-20 13:21:40 +0100

    use a component to identify cesium camera

---
+++ Runtime/CesiumCamera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace CesiumForUnity
+{
+    //add this component to a camera to identify the camera to use by cesium
+    public class CesiumCamera : MonoBehaviour
+    {
+        public static Camera camera;
+
+        private void OnEnable()
+        {
+            //try to get the camera if any; 
+            camera = GetComponent<Camera>();
+            
+        }
+    }
+}
