d9b8693bfd6b87c3a1ed96ce2a9b712513536c68
Janine Liu <janine@cesium.com>
2024-09-26 15:36:33 -0400

    Doc adjustments

--- Runtime/CesiumSampleHeightResult.cs
+++ Runtime/CesiumSampleHeightResult.cs
@@ -8,34 +8,34 @@ namespace CesiumForUnity
     public class CesiumSampleHeightResult
     {
         /// <summary>
-        /// The positions and sampled heights. The X component is Longitude (degrees), the
-        /// Y component is Latitude (degrees), and the Z component is Height (meters) above
-        /// the ellipsoid (usually WGS84).
+        /// The positions and their sampled heights. The X component is Longitude (degrees),
+        /// the Y component is Latitude (degrees), and the Z component is Height (meters) 
+        /// above the ellipsoid (usually WGS84).
         /// </summary>
         /// <remarks>
         /// <para>
-        /// The longitudes and latitudes will match the values at the same index in the
-        /// original input positions. Each height will either be the height sampled
-        /// from the tileset at that position, or the original input height if the
-        /// height could not be sampled. To determine which, look at the value of
+        /// For each resulting position, its longitude and latitude values will match
+        /// values from its input. Its height will either be the height sampled from
+        /// the tileset at that position, or the original input height if the sample
+        /// was unsuccessful. To determine which, look at the value of
         /// <see cref="CesiumSampleHeightResult.heightSampled"/> at the same index.
         /// </para>
         /// <para>
-        /// The returned height is is measured from the ellipsoid, which is usually WGS84.
-        /// It should not be confused with a height about Mean Sea Level.
+        /// The returned height is measured from the ellipsoid (usually WGS84) and
+        /// should not be confused with a height above Mean Sea Level.
         /// </para>
         /// </remarks>
         public double3[] longitudeLatitudeHeightPositions { get; set; }
 
         /// <summary>
-        /// Specifies whether the height for the position at this index was sampled successfully.
+        /// Indicates whether the height for the position at the corresponding index was sampled
+        /// successfully.
         /// </summary>
         /// <remarks>
-        /// If true, <see cref="CesiumSampleHeightResult.longitudeLatitudeHeightPositions"/> has
-        /// a valid height sampled from the tileset at this index. If false, the height
-        /// could not be sampled for the position at this index, and so the height in
-        /// <see cref="CesiumSampleHeightResult.longitudeLatitudeHeightPositions"/> is unchanged
-        /// from the original input height.
+        /// If true, then the corresponding position in
+        /// <see cref="CesiumSampleHeightResult.longitudeLatitudeHeightPositions"/> uses
+        /// the height sampled from the tileset. If false, the height could not be sampled for 
+        /// the position, so its height is the same as the original input height.
         /// </remarks>
         public bool[] heightSampled { get; set; }
 
