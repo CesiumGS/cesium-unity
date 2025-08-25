88951abbeb5035ac332bb2b73c137a070cb8e9fc
Brendan Duncan <brendan.duncan@unity3d.com>
2025-08-25 08:53:23 -0600

    Change UnityTaskProcessor to use std::thread instead of System::Threading

--- native~/Shared/src/UnityTaskProcessor.cpp
+++ native~/Shared/src/UnityTaskProcessor.cpp
@@ -1,14 +1,18 @@
 #include "UnityTaskProcessor.h"
 
 #include <DotNet/System/Action.h>
-#include <DotNet/System/Threading/Tasks/Task.h>
+//#include <DotNet/System/Threading/Tasks/Task.h>
+#include <thread>
 
 using namespace DotNet;
 
 namespace CesiumForUnityNative {
 
 void UnityTaskProcessor::startTask(std::function<void()> f) {
-  System::Threading::Tasks::Task::Run(f);
+  // TODO: System::Threading does not work with WASM builds, and std::thread(f) will
+  // be slower because it will create a new thread each time. A thread pool would be better.
+  std::thread(f).detach();
+  //System::Threading::Tasks::Task::Run(f);
 }
 
 } // namespace CesiumForUnityNative
