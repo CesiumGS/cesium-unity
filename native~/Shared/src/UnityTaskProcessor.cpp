6d768f7b49333639fe1d10dd8ef996a18cc802f3
Brendan Duncan <brendan.duncan@unity3d.com>
2025-08-26 12:03:02 -0600

    Use a threadpool for UnityTaskProcessor wasm

--- native~/Shared/src/UnityTaskProcessor.cpp
+++ native~/Shared/src/UnityTaskProcessor.cpp
@@ -1,18 +1,113 @@
 #include "UnityTaskProcessor.h"
 
+#ifndef __EMSCRIPTEN__
 #include <DotNet/System/Action.h>
-//#include <DotNet/System/Threading/Tasks/Task.h>
-#include <thread>
+#include <DotNet/System/Threading/Tasks/Task.h>
+#endif // __EMSCRIPTEN__
 
-using namespace DotNet;
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
 
 namespace CesiumForUnityNative {
 
+#ifdef __EMSCRIPTEN__
+// Unity Wasm can't use C# System/Threads, so we need to implement our own thread pool.
+
+// How many threads to use in the pool. This is somewhat arbitrary; we
+// just don't want to create a new thread for every task, to keep the number
+// of threads under the Emscripten limit.
+static const int UNITY_THREAD_POOL_SIZE = 10;
+
+class UnityThreadPool {
+public:
+    UnityThreadPool(size_t threads);
+
+    ~UnityThreadPool();
+
+    void enqueue(std::function<void()> f);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
+};
+
+UnityThreadPool::UnityThreadPool(size_t threads)
+    : stop(false) {
+    for(size_t i = 0; i < threads; ++i) {
+        workers.emplace_back([this] {
+            while (true) {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    
+                    this->condition.wait(lock, [this] { 
+                        return this->stop || !this->tasks.empty(); 
+                    });
+
+                    if (this->stop && this->tasks.empty()) {
+                        return;
+                    }
+                    
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                }
+
+                task();
+            }
+        });
+    }
+}
+
+void UnityThreadPool::enqueue(std::function<void()> task) {
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        if (stop) {
+          return;
+        }
+        tasks.emplace(task);
+    }   
+    condition.notify_one();
+}
+
+UnityThreadPool::~UnityThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    
+    condition.notify_all();
+    
+    for (std::thread &worker: workers) {
+        worker.join();
+    }
+}
+
+UnityTaskProcessor::UnityTaskProcessor()
+  : _threadPool(new UnityThreadPool(UNITY_THREAD_POOL_SIZE)) {
+}
+
+UnityTaskProcessor::~UnityTaskProcessor() {
+  delete _threadPool;
+}
+#else
+UnityTaskProcessor::UnityTaskProcessor() {}
+
+UnityTaskProcessor::~UnityTaskProcessor() {}
+#endif // __EMSCRIPTEN__
+
 void UnityTaskProcessor::startTask(std::function<void()> f) {
-  // TODO: System::Threading does not work with WASM builds, and std::thread(f) will
-  // be slower because it will create a new thread each time. A thread pool would be better.
-  std::thread(f).detach();
-  //System::Threading::Tasks::Task::Run(f);
+#ifdef __EMSCRIPTEN__
+  _threadPool->enqueue(f);
+#else // __EMSCRIPTEN__
+  DotNet::System::Threading::Tasks::Task::Run(f);
+#endif
 }
 
 } // namespace CesiumForUnityNative
--- native~/Shared/src/UnityTaskProcessor.h
+++ native~/Shared/src/UnityTaskProcessor.h
@@ -4,9 +4,21 @@
 
 namespace CesiumForUnityNative {
 
+class UnityThreadPool;
+
 class UnityTaskProcessor : public CesiumAsync::ITaskProcessor {
 public:
+  UnityTaskProcessor();
+
+  virtual ~UnityTaskProcessor() override;
+
   virtual void startTask(std::function<void()> f) override;
+
+private:
+#ifdef __EMSCRIPTEN__
+  // Unity Wasm can't use C# System/Threads, so we need to implement our own thread pool.
+  UnityThreadPool* _threadPool = nullptr;
+#endif
 };
 
 } // namespace CesiumForUnityNative
