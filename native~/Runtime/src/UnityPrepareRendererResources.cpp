6b159f8ffe749195a6fd7896c4502afac3b73e73
David Lively <david.lively@bentley.com>
2025-09-23 13:47:38 -0500

    First commit

--- native~/Runtime/src/UnityPrepareRendererResources.cpp
+++ native~/Runtime/src/UnityPrepareRendererResources.cpp
@@ -101,34 +101,34 @@ std::vector<TIndex> generateIndices(const int32_t count) {
   return syntheticIndexBuffer;
 }
 
-template <typename TIndex>
-void computeFlatNormals(
-    uint8_t* pWritePos,
-    size_t stride,
-    TIndex* indices,
-    int32_t indexCount,
-    const AccessorView<UnityEngine::Vector3>& positionView) {
-
-  for (int i = 0; i < indexCount; i += 3) {
-
-    TIndex i0 = indices[i];
-    TIndex i1 = indices[i + 1];
-    TIndex i2 = indices[i + 2];
-
-    const glm::vec3& v0 =
-        *reinterpret_cast<const glm::vec3*>(&positionView[i0]);
-    const glm::vec3& v1 =
-        *reinterpret_cast<const glm::vec3*>(&positionView[i1]);
-    const glm::vec3& v2 =
-        *reinterpret_cast<const glm::vec3*>(&positionView[i2]);
-
-    glm::vec3 normal = glm::normalize(glm::cross(v1 - v0, v2 - v0));
-    for (int j = 0; j < 3; j++) {
-      *reinterpret_cast<glm::vec3*>(pWritePos) = normal;
-      pWritePos += stride;
-    }
-  }
-}
+// template <typename TIndex>
+// void computeFlatNormals(
+//     uint8_t* pWritePos,
+//     size_t stride,
+//     TIndex* indices,
+//     int32_t indexCount,
+//     const AccessorView<UnityEngine::Vector3>& positionView) {
+//
+//   for (int i = 0; i < indexCount; i += 3) {
+//
+//     TIndex i0 = indices[i];
+//     TIndex i1 = indices[i + 1];
+//     TIndex i2 = indices[i + 2];
+//
+//     const glm::vec3& v0 =
+//         *reinterpret_cast<const glm::vec3*>(&positionView[i0]);
+//     const glm::vec3& v1 =
+//         *reinterpret_cast<const glm::vec3*>(&positionView[i1]);
+//     const glm::vec3& v2 =
+//         *reinterpret_cast<const glm::vec3*>(&positionView[i2]);
+//
+//     glm::vec3 normal = glm::normalize(glm::cross(v1 - v0, v2 - v0));
+//     for (int j = 0; j < 3; j++) {
+//       *reinterpret_cast<glm::vec3*>(pWritePos) = normal;
+//       pWritePos += stride;
+//     }
+//   }
+// }
 
 namespace {
 
@@ -757,14 +757,14 @@ void loadPrimitive(
   }
 
   if (duplicateVertices) {
-    if (computeFlatNormals) {
-      ::computeFlatNormals(
-          pWritePos + normalByteOffset,
-          stride,
-          indices,
-          indexCount,
-          positionView);
-    }
+    // if (computeFlatNormals) {
+    //   ::computeFlatNormals(
+    //       pWritePos + normalByteOffset,
+    //       stride,
+    //       indices,
+    //       indexCount,
+    //       positionView);
+    // }
     for (int64_t i = 0; i < vertexCount; ++i) {
       TIndex vertexIndex = indices[i];
       *reinterpret_cast<Vector3*>(pWritePos) = positionView[vertexIndex];
@@ -849,7 +849,7 @@ void loadPrimitive(
             pBufferStart + colorByteOffset,
             stride,
             static_cast<size_t>(vertexCount),
-            computeFlatNormals,
+            duplicateVertices,
             indices});
   }
 
