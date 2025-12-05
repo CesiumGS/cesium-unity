ec3d89000371648d3132445763ddb44f2fd045f4
Kevin Ring <kevin@kotachrome.com>
2025-12-05 21:04:31 +1100

    vcpkg insists we set C_FLAGS even though there's no need.

--- native~/vcpkg/triplets/x64-windows-unity.cmake
+++ native~/vcpkg/triplets/x64-windows-unity.cmake
@@ -7,3 +7,4 @@ set(VCPKG_LIBRARY_LINKAGE static)
 # In Debug builds on Windows, the default _ITERATOR_DEBUG_LEVEL=2 makes things _really_ slow,
 # for little benefit. Use a more reasonable debug level.
 set(VCPKG_CXX_FLAGS_DEBUG "/D_ITERATOR_DEBUG_LEVEL=1")
+set(VCPKG_C_FLAGS_DEBUG "/D_ITERATOR_DEBUG_LEVEL=1")
