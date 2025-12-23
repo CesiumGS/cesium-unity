6e8ce59a031dc87097b482c7a7f77db46d17a9b4
Kevin Ring <kevin@kotachrome.com>
2025-12-23 22:56:56 +1100

    Fix typo.
    Co-authored-by: Janine Liu <32226860+j9liu@users.noreply.github.com>

--- native~/src/Runtime/UnityExternals.cpp
+++ native~/src/Runtime/UnityExternals.cpp
@@ -106,7 +106,7 @@ void initializeExternals() {
       DotNet::System::EventHandler([](const DotNet::System::Object& sender,
                                       const DotNet::System::EventArgs& e) {
         // This is hacky. We need to free the ObjectHandles for the two
-        // parameters before we call endCurrentAppDomain, or else we're assert
+        // parameters before we call endCurrentAppDomain, or else we assert
         // when we do it afterward because the handles are from the "wrong"
         // AppDomain and finalization hasn't started yet. To free the handles,
         // we need to cast away const.
