b90a4c497328be8bd952c91154e53cd448883017
Tim Moore <tim.moore@cesium.com>
2026-07-17 23:18:19 +0200

    Revert "Use new TreeView classes in Unity 6.4 newer"
    This reverts commit 2c29c7fa049d8a0861b7acf26936a0e0e9e3e930.
    
    Try using TreeView generic classes in older versions of Unity.

--- Source/Editor/CesiumIonAssetsWindow.cs
+++ Source/Editor/CesiumIonAssetsWindow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 
-#if UNITY_6000_4_OR_NEWER
+#if UNITY_6000_2_OR_NEWER
 using TreeViewItem = UnityEditor.IMGUI.Controls.TreeViewItem<int>;
 using TreeViewState = UnityEditor.IMGUI.Controls.TreeViewState<int>;
 #endif
