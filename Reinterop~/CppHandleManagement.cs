802ff85f0ae0bbb1c61f215f14c7d72d424c5829
Kevin Ring <Kevin.Ring@bentley.com>
2026-08-13 18:34:24 +1000

    Cleanup.

--- Reinterop~/CppHandleManagement.cs
+++ Reinterop~/CppHandleManagement.cs
@@ -6,55 +6,44 @@ namespace Reinterop
     {
         internal static void Generate(CppGenerationContext context, TypeToGenerate item, GeneratedResult result)
         {
-            GeneratedCppDeclaration declaration = result.CppDeclaration;
-            GeneratedCppDefinition definition = result.CppDefinition;
-
             // We only need handle management for non-static classes.
             if (item.Type.IsStatic)
                 return;
 
             // Only classes, delegates, and non-blittable struct wrappers have handles.
-            if (declaration.Type.Kind != InteropTypeKind.ClassWrapper &&
-                declaration.Type.Kind != InteropTypeKind.NonBlittableStructWrapper &&
-                declaration.Type.Kind != InteropTypeKind.Delegate)
+            if (result.Type.Kind != InteropTypeKind.ClassWrapper &&
+                result.Type.Kind != InteropTypeKind.NonBlittableStructWrapper &&
+                result.Type.Kind != InteropTypeKind.Delegate)
             {
                 return;
             }
 
-            CppType type = CppType.FromCSharp(context, item.Type);
             CppType objectHandleType = CppObjectHandle.GetCppType(context);
 
-            string templateSpecialization = "";
-            if (declaration.Type.GenericArguments != null && declaration.Type.GenericArguments.Count > 0)
-            {
-                templateSpecialization = $"<{string.Join(", ", declaration.Type.GenericArguments.Select(arg => arg.GetFullyQualifiedName()))}>";
-            }
-
             // The handle to the managed object
-            declaration.Elements.Add(new(
+            result.CppDeclaration.Elements.Add(new(
                 Content: $"{objectHandleType.GetFullyQualifiedName()} _handle;",
                 IsPrivate: true,
                 TypeDefinitionsReferenced: new[] { objectHandleType }
             ));
 
             // Construct from an object handle
-            CppInteropFunction objectHandleConstructorRecipe = new CppInteropFunction(context, result.Type, type.Name)
+            CppInteropFunction objectHandleConstructorRecipe = new CppInteropFunction(context, result.Type, result.Type.Name)
                 .Parameters([new CppInteropParameter("handle", objectHandleType.AsMovableParameterType())])
                 .Explicit(true)
                 .NoExcept(true)
                 .Static(true)
                 .MemberInitializers([ new CppMemberInitializer("_handle", new CppMove(new CppIdentifier("handle"))) ])
                 .DefinitionBody([]);
-
             result.InteropFunctions.Add(objectHandleConstructorRecipe);
 
-            CppInteropFunction nullConstructorRecipe = new CppInteropFunction(context, result.Type, type.Name)
+            // Construct from a null pointer
+            CppInteropFunction nullConstructorRecipe = new CppInteropFunction(context, result.Type, result.Type.Name)
                 .Parameters([new CppInteropParameter("handle", CppType.NullPointer)])
                 .NoExcept(true)
                 .Static(true)
                 .MemberInitializers([ new CppMemberInitializer("_handle", new CppIdentifier("handle")) ])
                 .DefinitionBody([]);
-
             result.InteropFunctions.Add(nullConstructorRecipe);
 
             // For simple types without an overloaded operator==, we can check
@@ -65,13 +54,12 @@ namespace Reinterop
             // that means a call into C#.
             bool hasOverloadedOperatorEquals = CSharpTypeUtility
                 .FindMembers(item.Type, "op_Equality")
-                .Where(
+                .Any(
                     op => op is IMethodSymbol method &&
                     method.ReturnType.SpecialType == SpecialType.System_Boolean &&
                     method.Parameters.Length == 2 &&
                     SymbolEqualityComparer.Default.Equals(method.Parameters[0].Type, method.ContainingType) &&
-                    SymbolEqualityComparer.Default.Equals(method.Parameters[1].Type, method.ContainingType))
-                .Any();
+                    SymbolEqualityComparer.Default.Equals(method.Parameters[1].Type, method.ContainingType));
 
             if (!hasOverloadedOperatorEquals)
             {
